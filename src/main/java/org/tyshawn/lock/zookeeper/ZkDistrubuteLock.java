8ac1a4653efd8f81764b0fdd56c991d83da7fd71
tyshawnlee <tyshawnlee@qq.com>
2020-03-12 18:32:45 +0800

    zk分布式锁

--- src/main/java/org/tyshawn/lock/zookeeper/ZkDistrubuteLock.java
+++ src/main/java/org/tyshawn/lock/zookeeper/ZkDistrubuteLock.java
@@ -1,132 +1,151 @@
 package org.tyshawn.lock.zookeeper;
 
+import java.util.List;
+import java.util.TreeSet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 基于临时顺序节点的ZooKeeper分布式锁
+ * 解决了羊群效应的问题和锁公平性的问题
+ * @author litianxiang
+ * @date 2020/3/12 16:08
+ */
 public class ZkDistrubuteLock {
-    private static Logger logger = LoggerFactory.getLogger(ZkDistrubuteLock.class);
+	private static Logger logger = LoggerFactory.getLogger(ZkDistrubuteLock.class);
 
-    private String lockRootPath = "/exclusive_lock";
-    private String tempLockPath = lockRootPath + "/lock";
-    private final CountDownLatch connectedSignal = new CountDownLatch(1);
-    private ZooKeeper zk;
+	/**
+	 * 分布式锁的根节点路径
+	 */
+	private String rootLockPath = "/exclusive_lock";
+	/**
+	 * 分布式锁节点路径
+	 */
+	private String lockPath;
+	/**
+	 * 分布式锁名
+	 */
+	private String lockName;
+	private ZooKeeper zk;
 
-    public ZkDistrubuteLock(String host) {
-        try {
-            zk = new ZooKeeper(host, 5000, new Watcher() {
-                public void process(WatchedEvent event) {
-                    if (event.getState() == Event.KeeperState.SyncConnected) {
-                        connectedSignal.countDown();
-                    }
-                }
-            });
-            //因为监听器是异步操作, 要保证监听器操作先完成, 即要确保先连接上ZooKeeper再返回实例.
-            connectedSignal.await();
+	/**
+	 * 连接zk, 并创建分布式锁的根节点
+	 * @param host zk服务地址
+	 * @param lockName 分布式锁名
+	 */
+	public ZkDistrubuteLock(String host, String lockName) {
+		try {
+			CountDownLatch connectedSignal = new CountDownLatch(1);
+			zk = new ZooKeeper(host, 5000, new Watcher() {
+				@Override
+				public void process(WatchedEvent event) {
+					if (event.getState() == Event.KeeperState.SyncConnected) {
+						connectedSignal.countDown();
+					}
+				}
+			});
+			//因为监听器是异步操作, 要保证监听器操作先完成, 即要确保先连接上ZooKeeper再返回实例.
+			connectedSignal.await();
 
-            //创建锁的根节点(持久节点)
-            if (zk.exists(lockRootPath, false) == null) {
-                zk.create(lockRootPath, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            }
-        } catch (Exception e) {
-            logger.error("connect zookeeper server error.");
-        }
+			//创建锁的根节点(持久节点)
+			if (zk.exists(rootLockPath, false) == null) {
+				zk.create(rootLockPath, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+			}
 
-    }
+			//指定分布式锁节点路径
+			this.lockName = lockName;
+		} catch (Exception e) {
+			logger.error("connect zookeeper server error.", e);
+		}
+	}
 
+	/**
+	 * 获取锁
+	 * 在业务中获取到锁后才能继续往下执行, 否则堵塞, 直到获取到锁
+	 */
+	public void getLock() {
+		try {
+			//创建分布式锁的临时顺序节点
+			lockPath = zk.create(rootLockPath + "/" + lockName, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
 
-    private void watchNode(final String nodePath, final Thread thread) throws InterruptedException {
-        try {
-            zk.exists(nodePath, new Watcher() {
-                public void process(WatchedEvent event) {
-                    if (event.getType() == Event.EventType.NodeDeleted) {
-                        logger.info("lock release.");
-                        thread.interrupt();
-                    }
-                    try {
-                        zk.exists(nodePath, true);
-                    } catch (InterruptedException e) {
-                        logger.error(e.getMessage(), e);
-                    } catch (KeeperException e) {
-                        logger.error(e.getMessage(), e);
-                    }
-                }
-            });
-        }catch (KeeperException e){
-            logger.error("watch node error.", e);
-        }
-    }
+			//取出所有分布式锁的临时顺序节点, 然后排序
+			List<String> children = zk.getChildren(rootLockPath, false);
+			TreeSet<String> sortedChildren = new TreeSet<>();
+			for (String child : children) {
+				sortedChildren.add(rootLockPath + "/" + child);
+			}
 
-    /**
-     * 获取锁
-     * @return
-     * @throws InterruptedException
-     */
-    public boolean getLock() throws InterruptedException {
-        createLockRootPath();
-        watchNode(tempLockPath, Thread.currentThread());
+			//如果当前客户端创建的顺序节点是第一个, 则获取到锁
+			String firstNode = sortedChildren.first();
+			if (firstNode.equals(lockPath)) {
+				return;
+			}
 
-        String path = null;
-        while (true) {
-            try {
-                path = zk.create(tempLockPath, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-            } catch (KeeperException e) {
-                logger.info(Thread.currentThread().getName() + " get lock fail.");
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException ex) {
-                    logger.info(Thread.currentThread().getName() + " notify.");
-                }
-            }
-            if (StringUtils.isNotBlank(path)) {
-                logger.info(Thread.currentThread().getName() + " get lock.");
-                return true;
-            }
-        }
-    }
+			//如果当前客户端没有获取到锁, 则在前一个临时顺序节点上加一个监听器
+			String lowerNode = sortedChildren.lower(lockPath);
+			CountDownLatch latch = new CountDownLatch(1);
+			if (StringUtils.isBlank(lowerNode)) {
+				return;
+			}
+			Stat stat = zk.exists(lowerNode, new Watcher() {
+				@Override
+				public void process(WatchedEvent event) {
+					//当前一个临时顺序节点被删除后, 当前客户端就获取到锁(这样就保证了锁的公平性)
+					if (event.getType() == Event.EventType.NodeDeleted) {
+						latch.countDown();
+					}
+				}
+			});
+			if (stat != null) {
+				latch.await();
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
+	/**
+	 * 释放锁
+	 */
+	public void releaseLock() {
+		try {
+			zk.delete(lockPath, -1);
+		} catch (Exception e) {
+			logger.error("release lock error.", e);
+		}
+	}
 
-    /**
-     * 释放锁
-     */
-    public void releaseLock() {
-        try {
-            zk.delete(tempLockPath, -1);
-            logger.info(Thread.currentThread().getName() + " release lock.");
-        } catch (InterruptedException e) {
-            logger.error("release lock error.", e);
-        } catch (KeeperException e) {
-            logger.error("release lock error.", e);
-        }
-    }
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		for (int i = 0; i < 5; i++) {
+			executorService.execute(new Runnable() {
+				@Override
+				public void run() {
+					ZkDistrubuteLock zkDistrubuteLock = new ZkDistrubuteLock("xxx.xx.xx.xxx:2181", "myLock");
 
-
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 5; i++) {
-            executorService.execute(new Runnable() {
-                public void run() {
-                    ZkDistrubuteLock lock = new ZkDistrubuteLock("114.55.27.138:2181");
-                    try {
-                        lock.getLock();
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    lock.releaseLock();
-                }
-            });
-        }
-        executorService.shutdown();
-    }
+					//获取锁, 没有获取到锁就一直等待
+					zkDistrubuteLock.getLock();
+					try {
+						logger.info(Thread.currentThread().getName() + " 进行扣减库存操作...");
+					} catch (Exception e) {
+						logger.error("处理业务逻辑报错", e);
+					}finally {
+						//释放锁
+						zkDistrubuteLock.releaseLock();
+					}
+				}
+			});
+		}
+	}
 }
